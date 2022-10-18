ce3ec030a920e18ca37af86cf3fed91f402e5836
Jafet2105 <jafet.gonzalez.garcia@est.una.ac.cr>
2022-10-18 13:59:26 -0600

    Update jquery_formato.js

--- ProyectoGimnasio/js/jquery_formato.js
+++ ProyectoGimnasio/js/jquery_formato.js
@@ -1,10 +1,10 @@
 
     $(document).ready(function(){
         $(".mascaratelefono").mask("0000-0000");
-        $(".mascarapeso").mask("00.0kg");
+        $(".mascarapeso").mask("999.9kg");
         $(".mascaraaltura").mask("0.00cm");
         $(".mascaramonto").mask("₡0000000000000000");
-        $(".mascaraimpuesto").mask("00.00%");
+        $(".mascaraimpuesto").mask("99.99%");
         $(".mascaranumcuenta").mask("AA00000000000000000000");
         $(".mascaracantidad").mask("9999");
         /*
