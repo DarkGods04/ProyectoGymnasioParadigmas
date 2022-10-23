7d63e590e8cfa571cc48a84ad3870bd807b72a07
Yairu4439 <97989212+Yairu4439@users.noreply.github.com>
2022-10-23 11:12:06 -0600

    Terminando lo de predicción

--- ProyectoGimnasio/data/prediccionFactura.php
+++ ProyectoGimnasio/data/prediccionFactura.php
@@ -1,66 +1,50 @@
 <?php
 
 require 'data.php';
-
 $con = new Data();
 $pdo = $con->Data();
-$conn = mysqli_connect($this->server, $this->user, $this->password, $this->db);
-$conn->set_charset('UTF8');
-
 $campo = $_POST["campo"];
-$querySelectCliente = "SELECT * FROM tbcliente WHERE tbclientenombre LIKE '%$campo%' OR tbclienteapellido1 LIKE '%$campo%' OR
-        tbclienteapellido2 LIKE '%$campo%';";
-$resultCliente = mysqli_query($conn, $querySelectCliente);
-$idCliente = 0;
-while ($rowCliente = mysqli_fetch_array($resultCliente)) {
-    if ($rowCliente['tbclienteactivo'] == 1) {
-        $idCliente = $rowCliente['tbclienteid'];
-    }
-}
-
-$querySelectInstructor = "SELECT * FROM tbinstructor WHERE tbinstructornombre LIKE '%$campo%' OR tbinstructorapellido LIKE '%$campo%';";
-$resultInstructor = mysqli_query($conn, $querySelectInstructor);
-$idInstructor = 0;
-while ($rowInstructor = mysqli_fetch_array($resultInstructor)) {
-    if ($rowInstructor['tbinstructoractivo'] == 1) {
-        $idInstructor = $rowInstructor['tbinstructorid'];
-    }
-}
-
-$querySelectModalidad = "SELECT * FROM tbpagomodalidad Where tbpagomodalidadnombre LIKE '%$campo%';";
-$resultModalidad = mysqli_query($conn, $querySelectModalidad);
-$idModalidad = 0;
-while ($rowModalidad = mysqli_fetch_array($resultModalidad)) {
-    if ($rowModalidad['tbpagomodalidadactivo'] == 1) {
-        $idModalidad = $rowModalidad['tbpagomodalidadid'];
-    }
-}
-
-$querySelectServicio = "SELECT * FROM tbservicio WHERE tbservicionombre LIKE '%$campo%';";
-$idServicio = 0;
-$resultServicio = mysqli_query($conn, $querySelectServicio);
-while ($rowServicio = mysqli_fetch_array($resultServicio)) {
-    if ($rowServicio['tbservicioactivo'] == 1) {
-        $idServicio = $rowServicio['tbservicioid'];
-    }
-}
-
-
-$querySelectImpuesto = "SELECT * FROM tbimpuestoventa WHERE tbimpuestoventadescripcion LIKE '%$campo%';";
-$resultImpuesto = mysqli_query($conn, $querySelectImpuesto);
-$idImpuesto = 0;
-while ($rowimpuesto = mysqli_fetch_array($resultImpuesto)) {
-    if ($rowimpuesto['tbimpuestoventaactivo'] == 1) {
-        $idImpuesto = $rowimpuesto['tbimpuestoventaid'];
-    }
-}
-
-$sql =  "SELECT * FROM tbfactura WHERE tbfacturaid LIKE '%$campo%' OR tbfacturaclienteid LIKE '%$idCliente%' OR tbfacturainstructorid LIKE '%$idInstructor%' OR tbfacturafechapago LIKE '%$campo%' OR tbfacturapagomodalidad LIKE '%$idModalidad%' OR tbfacturaservicios LIKE '%$idServicio%' OR tbfacturaimpuestoventaid LIKE '%$idImpuesto%' OR tbfacturamontobruto LIKE '%$campo%' OR tbfacturamontoneto LIKE '%$campo%';";
-$query = $pdo->prepare($sql);
-$query->execute([$campo . '%']);
-
 $html = "";
+
+$sqlCliente = "SELECT  tbclienteid, tbclientenombre, tbclienteapellido1,tbclienteapellido2 FROM tbcliente WHERE (tbclienteactivo=1) AND tbclientenombre LIKE ?  OR tbclienteapellido1 LIKE ?  OR
+tbclienteapellido2 LIKE ? ORDER BY tbclienteid ASC LIMIT 0, 10";
+$query = $pdo->prepare($sqlCliente);
+$query->execute([$campo . '%', $campo . '%', $campo . '%']);
 while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-    $html .= "<li onclick=\"mostrar('" . $row["tbfacturaid"] . "')\">" . $row["tbfacturaid"] . "</li>";
+    $html .= "<li onclick=\"mostrar('" . $row["tbclientenombre"] . "')\">" . $row["tbclientenombre"] . " " . $row["tbclienteapellido1"] . " " . $row["tbclienteapellido2"] . "</li>";
 }
+
+$sqlInstructor = "SELECT tbinstructorid, tbinstructornombre, tbinstructorapellido FROM tbinstructor 
+WHERE (tbinstructoractivo=1) AND (tbinstructornombre LIKE ? OR tbinstructorapellido LIKE ?) ORDER BY tbinstructorid ASC LIMIT 0, 10";
+$query = $pdo->prepare($sqlInstructor);
+$query->execute([$campo . '%', $campo . '%']);
+while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+    $html .= "<li onclick=\"mostrar('" . $row["tbinstructornombre"] . "')\">" . $row["tbinstructornombre"] . " " . $row["tbinstructorapellido"] . "</li>";
+}
+
+$sqlModalidad = "SELECT tbpagomodalidadid, tbpagomodalidadnombre FROM tbpagomodalidad 
+WHERE (tbpagomodalidadactivo=1) AND (tbpagomodalidadnombre LIKE ?) ORDER BY tbpagomodalidadid ASC LIMIT 0, 10";
+$query = $pdo->prepare($sqlModalidad);
+$query->execute([$campo . '%']);
+while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+    $html .= "<li onclick=\"mostrar('" . $row["tbpagomodalidadnombre"] . "')\">" . $row["tbpagomodalidadnombre"] . "</li>";
+}
+
+
+$sqlServicio = "SELECT tbservicioid, tbservicionombre FROM tbservicio WHERE (tbservicioactivo=1) AND 
+(tbservicionombre LIKE ?) ORDER BY tbservicioid ASC LIMIT 0, 10";
+$query = $pdo->prepare($sqlServicio);
+$query->execute([$campo . '%']);
+while($row = $query-> fetch(PDO::FETCH_ASSOC)){
+    $html .= "<li onclick=\"mostrar('" . $row["tbservicionombre"] . "')\">" . $row["tbservicionombre"] . "</li>";
+}
+
+$sqlImpuesto = "SELECT tbimpuestoventaid, tbimpuestoventadescripcion FROM tbimpuestoventa WHERE (tbimpuestoventaactivo=1) AND 
+(tbimpuestoventadescripcion LIKE ?) ORDER BY tbimpuestoventaid ASC LIMIT 0, 10";
+$query = $pdo->prepare($sqlImpuesto);
+$query->execute([$campo .'%']);
+while($row = $query-> fetch(PDO::FETCH_ASSOC)){
+    $html .= "<li onclick=\"mostrar('" . $row["tbimpuestoventadescripcion"] . "')\">" . $row["tbimpuestoventadescripcion"] . "</li>";
+}
+
 echo json_encode($html, JSON_UNESCAPED_UNICODE);
