261a142053dfcc9f686564b97b44f9483a93c76f
Jafet2105 <jafet.gonzalez.garcia@est.una.ac.cr>
2022-10-27 22:11:48 -0600

    Revert "Revert "Merge branch 'main' of https://github.com/DarkGods04/ProyectoGymnasioParadigmas""
    This reverts commit 55201dd9d7937c0b849ac325b7726df3e6e2e1d8.

--- ProyectoGimnasio/business/servicioAction.php
+++ ProyectoGimnasio/business/servicioAction.php
@@ -1,5 +1,6 @@
 <?php
 include 'servicioBusiness.php';
+//Arturo Elizondo
 
 if (isset($_POST["insertar"])) {
     if (isset($_POST["nombreServicio"]) && isset($_POST["descripcionServicio"]) && isset($_POST["montoServicio"])) {
@@ -16,17 +17,18 @@ if (isset($_POST["insertar"])) {
                 $servicioBusiness = new servicioBusiness();
                 $result = $servicioBusiness->insertar($servicio);
 
-            if ($result == 1) {
-                header("location: ../view/listarServicios.php?success=updated");
-            } else {
-                header("location: ../view/listarServicios.php?error=dbError");
-            }
+                if ($result == 1) {
+                    header("location: ../view/listarServicios.php?success=updated");
+                } else {
+                    header("location: ../view/listarServicios.php?error=dbError");
+                }
             
+            } else {
+                header("location: ../view/listarServicios.php?error=emptyField");
+            }
         } else {
-            header("location: ../view/listarServicios.php?error=emptyField");
+            header("location: ../view/listarServicios.php?error=error");
         }
-    } else {
-        header("location: ../view/listarServicios.php?error=error");
     }
 }
 
@@ -50,29 +52,32 @@ if (isset($_POST['eliminar'])) {
 if (isset($_POST['actualizar'])) {
     if (isset($_POST['idServicio']) && isset($_POST['nombreServicio']) && isset($_POST['descripcionServicio']) && isset($_POST['montoServicio'])) {
 
-        $anteriorMontoServicio = $_POST['anteriorMontoServicio'];
-        $id = $_POST['idServicio'];
-        $nombreServicio = $_POST['nombreServicio'];
-        $descripcionServicio = $_POST['descripcionServicio'];
-        $montoServicio = $_POST["montoServicio"];
+            $anteriorMontoServicio = $_POST['anteriorMontoServicio'];
+            $id = $_POST['idServicio'];
+            $nombreServicio = $_POST['nombreServicio'];
+            $descripcionServicio = $_POST['descripcionServicio'];
+            $montoServicio = $_POST["montoServicio"];
 
-        if (strlen($nombreServicio) > 0 && strlen($descripcionServicio) > 0 && strlen($montoServicio) > 0) {
-            //$tempMonto = str_replace("₡","",$montoServicio);
+            if (strlen($nombreServicio) > 0 && strlen($descripcionServicio) > 0 && strlen($montoServicio) > 0) {
+                //$tempMonto = str_replace("₡","",$montoServicio);
 
-            if (is_numeric($montoServicio)) {
-                $servicio = new Servicio($id, $nombreServicio, $descripcionServicio, $montoServicio, 1);
-                $servicioBusiness = new servicioBusiness();
-                $result = $servicioBusiness->update($servicio, $anteriorMontoServicio)
+                if (is_numeric($montoServicio)) {
+                    $servicio = new Servicio($id, $nombreServicio, $descripcionServicio, $montoServicio, 1);
+                    $servicioBusiness = new servicioBusiness();
+                    $result = $servicioBusiness->update($servicio, $anteriorMontoServicio);
 
-            if ($result == 1) {
-                header("location: ../view/listarServicios.php?success=updated");
+                    if ($result == 1) {
+                        header("location: ../view/listarServicios.php?success=updated");
+                    } else {
+                        header("location: ../view/listarServicios.php?error=dbError");
+                    }
+                } else {
+                    header("location: ../view/listarServicios.php?error=numberFormat");
+                }
             } else {
-                header("location: ../view/listarServicios.php?error=dbError");
+                header("location: ../view/listarServicios.php?error=emptyField");
             }
             
-        } else {
-            header("location: ../view/listarServicios.php?error=emptyField");
-        }
     } else {
         header("location: ../view/listarServicios.php?error=error");
     }
