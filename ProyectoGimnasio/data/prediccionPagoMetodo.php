9b9aac71fdd65fda9702bf8fa7ad127458f0b90a
Yairu4439 <97989212+Yairu4439@users.noreply.github.com>
2022-10-30 06:59:28 -0600

    Update prediccionPagoMetodo.php

--- ProyectoGimnasio/data/prediccionPagoMetodo.php
+++ ProyectoGimnasio/data/prediccionPagoMetodo.php
@@ -11,10 +11,10 @@ $sql = "SELECT tbcatalogopagometodoid, tbcatalogopagometodonombre FROM tbcatalog
 WHERE (tbcatalogopagometodoactivo=1) AND (tbcatalogopagometodoid LIKE ? OR tbcatalogopagometodonombre LIKE ?) ORDER BY tbcatalogopagometodoid ASC LIMIT 0, 10";
 $query = $pdo->prepare($sql);
 $query->execute([$campo . '%', $campo . '%']);
-$html = "";
 
+$html = "";
 while($row = $query-> fetch(PDO::FETCH_ASSOC)){
-    $html .= "<li onclick=\"mostrar('".$row["tbcatalogopagometodonombre"]."')\">" . $row["tbcatalogopagometodoid"] . " - " . $row["tbcatalogopagometodonombre"] . "</li>";
+    $html .= "<br><br><br><li onclick=\"mostrar('".$row["tbcatalogopagometodonombre"]."')\">" . $row["tbcatalogopagometodoid"] . " - " . $row["tbcatalogopagometodonombre"] . "</li>";
 } 
 
 echo json_encode($html, JSON_UNESCAPED_UNICODE);
